a7b944b9707ed2fa654eca4193bf9071c6fb126b
Hiroyuki Koda <95255730+HIROS-DEV@users.noreply.github.com>
2023-02-24 12:16:00 +0900

    Add game function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getRandomNumber() {
 	return Math.floor(Math.random() * 3);
 }
@@ -37,20 +40,26 @@ function getPlayerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection === 'rock' && computerSelection === 'scissors') {
+		playerScore += 1;
 		return 'You Win! Rock beats Scissors';
 	} else if (playerSelection === 'rock' && computerSelection === 'paper') {
+		computerScore += 1;
 		return 'You Lose! Rock beats Paper';
 	} else if (playerSelection === 'rock' && computerSelection === 'rock') {
 		return 'Even! Rock evens Rock';
 	} else if (playerSelection === 'paper' && computerSelection === 'rock') {
+		playerScore += 1;
 		return 'You Win! Paper beats Rock';
 	} else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+		computerScore += 1;
 		return 'You Lose! Paper beats Scissors';
 	} else if (playerSelection === 'paper' && computerSelection === 'paper') {
 		return 'Even! Paper evens Paper';
 	} else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+		playerScore += 1;
 		return 'You Win! Scissors beats Paper';
 	} else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+		computerScore += 1;
 		return 'You Lose! Scissors beats Rock';
 	} else if (
 		playerSelection === 'scissors' &&
@@ -58,15 +67,30 @@ function playRound(playerSelection, computerSelection) {
 	) {
 		return 'Even! Scissors evens Scissors';
 	} else {
-		return 'Error! Sorry, something happens';
+		return;
 	}
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
+function game() {
+	for (let i = 1; i <= 5; i++) {
+		const playerSelection = getPlayerChoice();
+		const computerSelection = getComputerChoice();
 
-// Create single playRound function. The function should take two parameters - playerSelection and computerSelection
-// This function returns a string  ex:"You Lose! Paper beats Rock"
+		if (playerSelection === undefined) {
+			return;
+		}
+		console.log(playerSelection, computerSelection);
+		console.log(playRound(playerSelection, computerSelection));
+		console.log(playerScore, computerScore);
+	}
 
-// Write a game function. Call the playRound function inside of this one to play a 5 round game.
-// Show the score and reports a winner or loser at the end..
+	if (playerScore > computerScore) {
+		return `You Win! You're score: ${playerScore}. Computer score: ${computerScore}`;
+	} else if (playerScore < computerScore) {
+		return `You Lose! You're score: ${playerScore}. Computer score: ${computerScore}`;
+	} else {
+		return `Even! You're score: ${playerScore}. Computer score: ${computerScore}`;
+	}
+}
+
+console.log(game());
